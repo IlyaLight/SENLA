cadb34021f755debdce88cb99ac56ff9d040424a
IlyaLight <@88light88@gmail.comgit config --global user.email @88light88@gmail.com>
2017-10-02 17:21:28 +0300

    исправления

--- task-4/BooksShop/src/com/senla/booksshop/Main.java
+++ task-4/BooksShop/src/com/senla/booksshop/Main.java
@@ -1,13 +1,16 @@
 package com.senla.booksshop;
 
 
-import com.senla.booksshop.collection.CollectionOrder;
-import com.senla.booksshop.collection.CollectionRequest;
-import com.senla.booksshop.objekt.Book;
-import com.senla.booksshop.objekt.Order;
-import com.senla.booksshop.objekt.Request;
-import com.senla.booksshop.collection.CollectionBook;
-import com.senla.booksshop.sorted.SortedBook;
+import com.senla.booksshop.controller.Shop;
+import com.senla.booksshop.stores.OrderCollection;
+import com.senla.booksshop.stores.RequestCollection;
+import com.senla.booksshop.model.Book;
+import com.senla.booksshop.model.Order;
+import com.senla.booksshop.model.Request;
+import com.senla.booksshop.stores.BookCollection;
+import com.senla.booksshop.servis.BookSorter;
+import com.senla.booksshop.utility.Printer;
+import com.senla.booksshop.utility.WorkWithFile;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -15,35 +18,34 @@ import java.util.Date;
 
 public class Main {
 
+    static final String  STANDART_PATH = "d:/";
+
     public static void main(String[] args) {
 
-        final String  STANDART_PATH = "d:";
-
         System.out.println("Book_Shop");
-        System.out.println("args0 = " + args[0]);
         String path = STANDART_PATH;
-        if (args[0] != null) {
+        if (args.length > 0) {
+            System.out.println("args0 = " + args[0]);
             path = args[0];
         }
-        //testCreate("d:/");
-        CollectionBook collectionBook = new CollectionBook();
-        collectionBook.readFromFile(args[0] + "/books.txt");
-        CollectionRequest collectionRequest = new CollectionRequest();
-        CollectionOrder collectionOrder = new CollectionOrder();
-        SortedBook sortedBook = new SortedBook(collectionBook.getBookList());
-        Print.printCollection(sortedBook.getBooksSortedByName());
+        //testCreate(path);
+        testReadWritetoFile(path);
     }
 
     public static void testCreate(String path){
-        CollectionBook collectionBook = new CollectionBook();
-        ArrayList<Book> books = collectionBook.getBookList();
-        books.add(new Book("x", new Date(1962, 2, 1), (float)151.12, false));
-        books.add(new Book("a", new Date(1962, 2, 1), (float)90, true));
-        books.add(new Book("b", new Date(1970, 2, 1), (float)151, true));
-        books.add(new Book("c", new Date(1989, 2, 1), (float)40.5, true));
+        Shop shop = new Shop();
 
-        CollectionRequest collectionRequest = new CollectionRequest();
-        ArrayList<Request> requests = collectionRequest.getRequestArrayList();
+        BookCollection bookCollection = new BookCollection();
+        ArrayList<Book> books = bookCollection.getBookList();
+        books.add(new Book("x", new Date(1962, 2, 1), (float)151.12, 1));
+        books.add(new Book("a", new Date(1962, 2, 1), (float)90, 0));
+        books.add(new Book("b", new Date(1970, 2, 1), (float)151, 11));
+        books.add(new Book("c", new Date(1989, 2, 1), (float)40.5, 3));
+        BookCollection bookCollection1 = new BookCollection();
+        bookCollection.setBookList(books);
+
+        RequestCollection requestCollection = new RequestCollection();
+        ArrayList<Request> requests = requestCollection.getRequestArrayList();
         requests.add(new Request("x", true));
         requests.add(new Request("x", false));
         requests.add(new Request("c", true));
@@ -55,16 +57,26 @@ public class Main {
         requests.add(new Request("b", true));
         requests.add(new Request("x", false));
 
-        CollectionOrder collectionOrder = new CollectionOrder();
-        ArrayList<Order> orders = collectionOrder.getOrderArrayList();
+        OrderCollection orderCollection = new OrderCollection();
+        ArrayList<Order> orders = orderCollection.getOrderArrayList();
         orders.add(new Order("c", (float)50, new Date(), "transit", false));
         orders.add(new Order("c", (float)40, new Date(), "transit", false));
         orders.add(new Order("c", (float)40, new Date(), "transit", false));
         orders.add(new Order("x", (float)150, new Date(), "delivery", true));
 
-        collectionOrder.writeToFile(path + "orders.txt");
-        collectionRequest.writeToFile(path + "request.txt");
-        collectionBook.writeToFile(path + "books.txt");
+        shop.setBookCollection(bookCollection);
+        shop.setOrderCollection(orderCollection);
+        shop.setRequestCollection(requestCollection);
+
+        shop.writeToFile(path);
 
     }
+
+    public static void testReadWritetoFile(String path){
+        Shop shop = new Shop();
+        shop.readFromFile(path);
+        shop.writeToFile(path);
+        Printer.printCollection(BookSorter.getBooksSortedByName(shop.getBookCollection().getBookList()));
+    }
+
 }
