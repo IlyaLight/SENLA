04a4426e533584bd5fb0c41a8f0c0bd90b612462
IlyaLight <@88light88@gmail.comgit config --global user.email @88light88@gmail.com>
2017-12-31 15:19:55 +0300

    up

--- book_shop/back/src/main/java/com/senla/booksshop/utility/JPAUtil.java
+++ book_shop/back/src/main/java/com/senla/booksshop/utility/JPAUtil.java
@@ -1,5 +1,8 @@
 package com.senla.booksshop.utility;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
@@ -7,9 +10,12 @@ public class JpaUtil {
     private static final String PERSISTENCE_UNIT_NAME = "PERSISTENCE";
     private static EntityManagerFactory factory;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(JpaUtil.class);
+
     public static EntityManagerFactory getEntityManagerFactory() {
         if (factory == null) {
             factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+            LOGGER.info("create Entity Manager Factory");
         }
         return factory;
     }
