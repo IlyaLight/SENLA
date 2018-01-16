ed1bc4506c4171900367cdc83441d2fca731b31c
IlyaLight <@88light88@gmail.comgit config --global user.email @88light88@gmail.com>
2018-01-16 08:25:29 +0300

    up

--- book-shop/back/src/main/java/com/senla/booksshop/dao/realization/AbstractJpaHibernateDao.java
+++ book-shop/back/src/main/java/com/senla/booksshop/dao/realization/AbstractJpaHibernateDao.java
@@ -19,9 +19,6 @@ import java.util.List;
 
 public abstract class AbstractJpaHibernateDao<T extends IModel, PK> implements IGenericDao<T, PK> {
 
-    private static final String ORDER_BY = " order by ";
-    private static final String WHERE_ID_PARAM_ID = " where id = :paramId ";
-
 
     public abstract String getSelectQuery();
 
@@ -31,30 +28,22 @@ public abstract class AbstractJpaHibernateDao<T extends IModel, PK> implements I
 
     @Override
     public void create(T object) {
-        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        session.persist(object);
-        session.getTransaction().commit();
+        EntityManager em = HibernateUtil.getEm();
+        em.getTransaction().begin();
+        em.persist(object);
+        em.getTransaction().commit();
     }
 
     @Override
     public T getByPK(int key) {
-//        Session session = HibernateUtil.getSessionFactory().openSession();
-//        Criteria criteria = session.createCriteria(getClazz())
-//                .add(Restrictions.eq("id", key));
-//        T t = (T)criteria.uniqueResult();
-//        session.close();
-//        return t;
+
         CriteriaBuilder builder = HibernateUtil.getEm().getCriteriaBuilder();
         CriteriaQuery<T> criteria = builder.createQuery( getClazz() );
         Root<T> root = criteria.from( getClazz() );
         criteria.select( root )
             .where(builder.equal(root.get(mmGetID()), key));
-        return (T)HibernateUtil.getEm().createQuery(criteria).getSingleResult();
+        return HibernateUtil.getEm().createQuery(criteria).getSingleResult();
 
-//        return (T)HibernateUtil.getEm().createQuery(getSelectQuery() + WHERE_ID_PARAM_ID)
-//                .setParameter("paramId", key)
-//                .getSingleResult();
     }
 
     @Override
@@ -63,34 +52,20 @@ public abstract class AbstractJpaHibernateDao<T extends IModel, PK> implements I
         em.getTransaction().begin();
         em.merge(object);
         em.getTransaction().commit();
-
-//        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-//        session.beginTransaction();
-//        session.update(object);
-//        session.getTransaction().commit();
     }
 
     @Override
     public void delete(T object) {
         CriteriaBuilder builder = HibernateUtil.getEm().getCriteriaBuilder();
+        HibernateUtil.getEm().getTransaction().begin();
         CriteriaDelete<T> criteria = builder.createCriteriaDelete( getClazz() );
         Root<T> root = criteria.from( getClazz() );
         criteria.where(builder.equal(root.get(mmGetID()), object.getId()));
-
-//        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-//        session.beginTransaction();
-//        session.delete(object);
-//        session.getTransaction().commit();
+        HibernateUtil.getEm().getTransaction().commit();
     }
 
     @Override
     public List getAll(String sortingColumn) {
-//        Session session = HibernateUtil.getSessionFactory().openSession();
-//        List tList = session.createCriteria(getClazz())
-//                .addOrder(Order.asc(sortingColumn))
-//                .list();
-//        session.close();
-//        return tList;
 
         CriteriaBuilder builder = HibernateUtil.getEm().getCriteriaBuilder();
         CriteriaQuery<T> criteria = builder.createQuery( getClazz() );
@@ -98,9 +73,6 @@ public abstract class AbstractJpaHibernateDao<T extends IModel, PK> implements I
         criteria.select( root )
             .groupBy(root.get(sortingColumn));
         return HibernateUtil.getEm().createQuery(criteria).getResultList();
-
-            //return HibernateUtil.getEm().createQuery(ORDER_BY + sortingColumn).getResultList();
-
 
     }
 }
---
+++ book-shop/back/src/main/java/com/senla/booksshop/utility/PropertiesHolder.java
@@ -0,0 +1,39 @@
+package com.senla.booksshop.utility;
+
+import dependencyinjection.annotation.ConfigProperty;
+
+public class PropertiesHolder
+{
+    @ConfigProperty(type = int.class)
+    private int staleTime;
+
+    @ConfigProperty(type = boolean.class)
+    private boolean automaticallyExecuteRequest;
+
+    @ConfigProperty(type = String.class)
+    private String csvPath;
+
+    public String getCsvPath() {
+        return csvPath;
+    }
+
+    public void setCsvPath(String serializablePath) {
+        this.csvPath = serializablePath;
+    }
+
+    public int getStaleTime() {
+        return staleTime;
+    }
+
+    public void setStaleTime(int staleTime) {
+        this.staleTime = staleTime;
+    }
+
+    public boolean isAutomaticallyExecuteRequest() {
+        return automaticallyExecuteRequest;
+    }
+
+    public void setAutomaticallyExecuteRequest(boolean automaticallyExecuteRequest) {
+        this.automaticallyExecuteRequest = automaticallyExecuteRequest;
+    }
+}
